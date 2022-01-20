2f60f9fd1318534b128f43e9e9fd033211e436f5
sugi511 <nori.setalab@gmail.com>
2022-01-20 17:33:15 +0900

    userの新規作成と表示部

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.fetch(:user, {}).permit(:company_id, :name, :api_key, :api_secret)
     end
 end
