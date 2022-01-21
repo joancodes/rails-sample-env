3b7715e3ce444b5783df83956d267e14a6d62a6a
sugi511 <nori.setalab@gmail.com>
2022-01-21 15:08:40 +0900

    Companyごとにresourceにアクセスできるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_company
+    @current_company ||= Company.find(params[:company_id]) if params[:company_id]
+  end
+
+  helper_method :current_company
 end
