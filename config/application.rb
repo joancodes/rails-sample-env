c654b46cd09f43c6e56a915a554e625e9ec6f780
sugi511 <nori.setalab@gmail.com>
2022-01-21 17:16:51 +0900

    api authのroutingの実装

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RailsGraphqlSample
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.paths["config/routes.rb"].concat(Dir[Rails.root.join("config/routes/**/*.rb")])
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
