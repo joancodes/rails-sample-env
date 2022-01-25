5e75070e2b9e2d86720f97f68ad4c40e148f27fa
sugi511 <nori.setalab@gmail.com>
2022-01-25 14:49:58 +0900

    users APIの実装

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, { format: 'json' } do
     namespace :v1 do
       post 'auth', to: 'sessions#create'
+      resources :users, only: [:index, :show]
     end
   end
 end
