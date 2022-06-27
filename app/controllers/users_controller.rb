81007b8c51bf7d794652d422ef5c5864f9e9f68c
sugi511 <nori.setalab@gmail.com>
2022-06-27 17:04:44 +0900

    OTPのページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ show edit update destroy otp ]
 
   # GET /users or /users.json
   def index
@@ -59,6 +59,10 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def otp
+
   end
 
   private
